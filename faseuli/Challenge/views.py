05cd9f696d73e5bf9a1c84c8ffeb9183360e0d8e
rxb8k <12183887@inha.edu>
2021-09-24 20:26:23 +0900

    2021. 09. 24 20: 26 for merge

--- faseuli/Challenge/views.py
+++ faseuli/Challenge/views.py
@@ -1,3 +1,14 @@
+from faseuli.Challenge.models import Challenge
 from django.shortcuts import render
 
 # Create your views here.
+def newChallenge(req):
+    Challenge_object=Challenge()
+    if req.method=='POST':
+        Challenge_object.title=req.POST['title']
+        Challenge_object.cost=req.POST['cost']
+        Challenge_object.goal=req.POST['goal']
+        Challenge_object.now=req.POST['now']
+        Challenge_object.save()
+        return redirect('/challenge/main')
+    return render(req,'.html', {'data': Challenge_object})
