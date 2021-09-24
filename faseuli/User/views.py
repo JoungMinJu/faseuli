1c6d424935b93bebe1929d738e19129fb5e1b588
rxb8k <12183887@inha.edu>
2021-09-24 16:46:57 +0900

    2021. 09. 24 16:46

--- faseuli/User/views.py
+++ faseuli/User/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import auth
+from .models import User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+#회원가입
+def signup_view(request):
+    res_data = {}
+    if request.method =='POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+            username = request.POST['username'], 
+            password=request.POST['password1'], 
+            nickname= request.POST['nickname'],
+            age=request.POST['age'],
+            hobby=request.POST['hobby'],
+        )
+        user.save()
+        auth.login(request, user)
+        return redirect('/login')
+    else:
+        res_data['error'] = '비밀번호가 다릅니다.'
+    return render(request, 'signup.html', res_data)
+
+#로그인
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username = username, password = password)
+        print(user)
+    if user is not None:
+        auth.login(request, user)
+        return redirect('/main')
+    return render(request, '.html')
+
+#로그아웃
+@login_required
+def logout_view(request):
+    auth.logout(request)
+    return redirect('')
