c6a211b94fc168a646c76d839b2911176bd26550
rxb8k <12183887@inha.edu>
2021-09-25 10:22:54 +0900

    10:22 url 추가

--- faseuli/Challenge/urls.py
+++ faseuli/Challenge/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('main', views.challenge_main, name="main"),
     path('detail/<int:id>', views.challenge_detail, name="detail"),
     path('write/<int:id>', views.challenge_write, name="write"),
+    path('new_history/<int:id>', views.new_history, name="new_history")
 ]
