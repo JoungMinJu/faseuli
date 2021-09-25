b459d06b6b3b81e971a53f06e67481a0d5c6fe4f
JoungMinJu <alswn9938@gmail.com>
2021-09-25 11:12:34 +0900

    11:12 Plan urls.py 수정

--- faseuli/Plan/urls.py
+++ faseuli/Plan/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('main/', plan_main, name="main"),
     path('add/', plan_add, name="add"),
     path('create/',plan_create, name='create'),
-    path('del/',plan_del,name='del'),
-    path('edit/',edit_plan, name='edit'),
-    path('update/',update_plan, name='update'),
+    path('del/<str:id>',plan_del,name='del'),
+    path('edit/<str:id>',edit_plan, name='edit'),
+    path('update/<str:id>',update_plan, name='update'),
 ]
